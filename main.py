03eba8d45e796bfe1fff7c6cfee821570515afc2
Paul Rutledge <paul.v.rutledge@gmail.com>
2014-05-12 23:49:48 -0500

    update

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from weightings import *
 from strategy import *
 from game import *
 
-heuristic = Greedy()#Weighted(CornerEdgeOther(3,2,1).gen(8))
+heuristic = Weighted(CornerEdgeOther(3,2,1).gen(8))
 game = Standard(heuristic)
 game.animate(0.1)
